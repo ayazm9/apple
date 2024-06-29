f149445e7185b102d935adc928698c629045f063
Ayaz Memon <21bt04045@gsfcuniversity.ac.in>
2024-06-29 15:44:34 +0530

    Integrate Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
+import React, { useEffect } from 'react';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Fetures from "./components/Fetures";
+import HowitWorks from "./components/HowitWorks";
+import Footer from "./components/Footer";
+import * as Sentry from '@sentry/react';
+import ReactGA from 'react-ga';
+import { useLocation } from 'react-router-dom';
 
-import Highlights from "./components/Highlights"
-import Model from "./components/Model"
-import Fetures from "./components/Fetures"
-import HowitWorks from "./components/HowitWorks"
-import Footer from "./components/Footer"
+// Initialize Google Analytics
+ReactGA.initialize('G-NRNZKBHXQ6');
 
-import *as Sentry from '@sentry/react';
+const usePageTracking = () => {
+  const location = useLocation();
 
-const App =()=>{
-  
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+};
+
+const App = () => {
+  usePageTracking();
+
   return (
     <main className="bg-black">
-    <  Navbar/>
-    <  Hero/>
-    <  Highlights/>
-    < Model/>
-    < Fetures/>
-    <HowitWorks/>
-    < Footer/>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Fetures />
+      <HowitWorks />
+      <Footer />
     </main>
+  );
+};
 
-  )
-}  
-    
-    
-  
 export default Sentry.withProfiler(App);
